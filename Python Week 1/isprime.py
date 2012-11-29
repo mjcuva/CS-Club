6126b41da189ce116150d0ddd037f0bc1745044e
Marc <mjcuva@gmail.com>
2012-11-29 12:12:10 -0600

    First python example

---
+++ Python Week 1/isprime.py
@@ -0,0 +1,22 @@
+# Output To User
+number = input("Enter a number: ")
+
+# Start loop counter
+i = 2
+prime = True
+
+# Check every number to see if it is divisible
+while(i < number and prime == True):
+    if(number % i == 0):
+        prime = False
+        i += 1
+    else:
+        i += 1
+
+# If we couldn't find a divisible number
+if prime == True:
+    print str(number) + " is prime"
+
+# If we found a number
+else:
+    print str(number) + " ins't prime"
