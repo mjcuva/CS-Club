a786c7d599b64c8f30e0526cc92057f0cf02a30e
Marc <mjcuva@gmail.com>
2012-12-26 17:20:32 -0600

    Count squares twice

--- Python Week 1/countfactors.py
+++ Python Week 1/countfactors.py
@@ -8,8 +8,13 @@ count = 0
 # Check every number to see if it is divisible
 while(i <= number):
     if(number % i == 0):
-        count += 1
+        if(i * i == number):
+            count += 2
+        else:
+            count += 1
+        print i
 
     i += 1
 
 print str(number) + " has " + str(count) + " factors." 
+
