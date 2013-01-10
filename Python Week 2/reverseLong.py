137f467116cf6911d7d1eeb4d9aabca17f87d3e3
Marc <mjcuva@gmail.com>
2013-01-10 14:09:18 -0600

    "Long reverse"

---
+++ Python Week 2/reverseLong.py
@@ -0,0 +1,11 @@
+out = open('output.txt', "w")
+
+inFile = open('input.txt')
+
+lines = inFile.readlines() # Creates array on lines
+
+for line in lines:
+    newLine = line.split()[::-1] # The third number is a selector, 2 gives every other, -1 reverses
+    newLine = " ".join(newLine) # Join takes the array and places a space between every element
+    newLine += "\n" # Adds new line after each line
+    out.write(newLine) # Write the line to the output file
